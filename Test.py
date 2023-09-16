a20088ad79ce280a1f1eeccc973371e861252db1
konmar05 <mako96@hotmail.de>
2023-09-16 18:01:29 +0200

    update file Test.py

--- Test.py
+++ Test.py
@@ -8,6 +8,28 @@ import json
 # todo add parameter to specific algorithm (for example walkscan distance)
 def test_algorithm(algorithm):
     """
+    function to start twenty (20) test runs
+    choose from following algorithms
+
+        louvain
+
+        infomap
+
+        label_propagation
+
+        eigenvector
+
+        girvan_newman (1 to 20 iterations)
+
+        greedy_modularity
+
+        walktrap
+
+        async_fluid (try to find 1 to 20 communities)
+
+        walkscan
+
+        graph_entropy
 
     :param algorithm: choose algorithm, must be a string
     :return:
@@ -52,8 +74,9 @@ def test_algorithm(algorithm):
             if i > 2:
                 plot_size = (10, 10)
 
-            viz.plot_network_clusters(graph, com, f.get_layout(i), figsize=plot_size)
-            plt.savefig('pictures/' + name + algorithm + 'test_' + str(k) + '.png')
+            # just for save the graph plots
+            # viz.plot_network_clusters(graph, com, f.get_layout(i), figsize=plot_size)
+            # plt.savefig('pictures/' + name + algorithm + 'test_' + str(k) + '.png')
 
         graphs[name] = test_runs
 
