91210acc88a9a62a8a914dd22ce0a83fea86b7e7
konmar05 <mako96@hotmail.de>
2023-09-17 18:50:05 +0200

    file added for frequent used variables

---
+++ variables.py
@@ -0,0 +1,54 @@
+# import this file for easy use of variables
+
+# var for algorithms
+lv = 'louvain'
+
+lp = 'label_propagation'
+
+ev = 'eigenvector'
+
+im = 'infomap'
+
+gn = 'girvan_newman'
+
+wt = 'walktrap'
+
+gm = 'greedy_modularity'
+
+af = 'async_fluid'
+
+# var for specific modularity scores
+m_scores = 'modularity_scores'
+
+mod_er = 'erdos_renyi'
+
+mod_link = 'link'
+
+mod_density = 'modularity_density'
+
+mod_gn = 'girvan_newman'
+
+mod_z = 'z_modularity'
+
+# var for specific fitness scores
+f_scores = 'fitness_scores'
+
+f_com = 'communities'
+
+f_dist = 'avg_distance'
+
+f_embd = 'avg_embeddedness'
+
+f_iDeg = 'avg_internal_degree'
+
+f_edges = 'edges_inside'
+
+f_exp = 'expansion'
+
+f_iEDen = 'internal_edge_density'
+
+f_scDen = 'scaled_density'
+
+f_trans = 'avg_transitivity'
+
+f_size = 'size'
